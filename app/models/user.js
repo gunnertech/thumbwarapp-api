6b49af724460671e5732289a87c8989bedad9276
Cody Swann <cody@gunnertech.com>
2015-12-13 15:55:07 -0500

    first commit

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var bcrypt = require('bcrypt');
+var knox = require('knox');
 var SALT_WORK_FACTOR = 10;
 
 var UserSchema   = new Schema({
