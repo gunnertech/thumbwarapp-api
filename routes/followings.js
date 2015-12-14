043f9845d61481d1ea37b4098b6fe426c5df07c2
Cody Swann <cody@gunnertech.com>
2015-12-14 13:35:37 -0500

    first commit

--- routes/followings.js
+++ routes/followings.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Following = require('../app/models/following');
+// var Following = require('../app/models/following');
 var User = require('../app/models/user');
 var _ = require('lodash');
 
