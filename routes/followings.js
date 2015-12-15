d23271e8cabbfd940f88e3e6084a39ad1a4a8c0d
Cody Swann <cody@gunnertech.com>
2015-12-14 21:23:18 -0500

    first commit

--- routes/followings.js
+++ routes/followings.js
@@ -3,56 +3,55 @@ var router = express.Router();
 var Following = require('../app/models/following');
 var User = require('../app/models/user');
 var _ = require('lodash');
+var async = require('async');
 
 router.post('/batch', function(req, res) {
   console.log(req.body.facebookIds.split(","))
-  
-  User.find({
-    facebookId: { $in: req.body.facebookIds.split(",") }
-  }).exec()
-  .then(function(users){
-    console.log('WElll.......');
-    console.log(users);
-    
-    console.log("HERE IS THE " + users.length + "!!!");
-    console.log(users);
-    
-    if(!users.length) {
-      console.log("OK HERE IT IS")
-      res.json("");
-    } else {
-      console.log("WHAT????")
-      // res.json("weird");
-      // _.each(users,function(user){
-      //   Following.count({
-      //     followee: req.currentUser,
-      //     follower: user
-      //   }).exec()
-      //   .then(function(count){
-      //     if(count < 1) {
-      //       // user.follow(req.currentUser);
-      //     }
-      //
-      //     Following.find({
-      //       followee: user,
-      //       follower: req.currentUser
-      //     })
-      //     .then(function(followings){
-      //       if(followings.length < 1) {
-      //         // req.currentUser.follow(user);
-      //       }
-      //
-      //       res.json("");
-      //
-      //     });
-      //   });
-      // });
-    }
+
+  Following.find({follower: req.currentUser}).exec()
+  .then(function(followings){
+    return User.find({
+      facebookId: { $in: req.body.facebookIds.split(",") },
+      _id: {
+        $ne: _.map(followings,function(following){ return following.followee; })
+      }
+    }).exec()
+    .then(function(users){
+      return _.map(users,function(user){ return {follower: req.currentUser._id, followee: user} });
+    })
   })
+  .then(function(followingData){
+    return Following.create(followingData).exec()
+    .then(function(){
+      return null;
+    })
+  })
+  .then(function(){
+    return Following.find({followee: req.currentUser}).exec()
+    .then(function(followings){
+      return followings;
+    })
+  })
+  .then(function(followings){
+    return User.find({
+      facebookId: { $in: req.body.facebookIds.split(",") },
+      _id: {
+        $ne: _.map(followings,function(following){ return following.follower; })
+      }
+    }).exec()
+    .then(function(users){
+      return _.map(users,function(user){ return {followee: req.currentUser._id, followeer: user} });
+    })
+  })
+  .then(function(followingData){
+    return Following.create(followingData).exec()
+    .then(function(){
+      res.json("");
+    })
+  }) 
   .catch(function(err){
     console.log("THERE IS AN ERROR")
     console.log(err)
-    res.status(500).json(err);
   });
     
     
