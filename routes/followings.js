d0d86a5b601ea2d95bb9aecee657249d45e26f4c
Cody Swann <cody@gunnertech.com>
2015-12-14 20:13:56 -0500

    first commit

--- routes/followings.js
+++ routes/followings.js
@@ -7,15 +7,15 @@ var _ = require('lodash');
 router.post('/batch', function(req, res) {
   console.log(req.body.facebookIds)
   
-  User.find({
-    facebookId: { $in: req.body.facebookIds }
+  User.findOne({
+    facebookId: "dd"
   }).exec()
   .then(function(users){
     console.log('WElll.......');
     console.log(users);
     
-    console.log("HERE IS THE " + users.length + "!!!");
-    console.log(users);
+    // console.log("HERE IS THE " + users.length + "!!!");
+    // console.log(users);
     
     if(!users.length) {
       res.json("");
