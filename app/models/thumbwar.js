06c0d84fecc195e79810eb7c4c92c483bcecbf0e
Cody Swann <cody@gunnertech.com>
2015-12-16 17:14:12 -0500

    minor

--- app/models/thumbwar.js
+++ app/models/thumbwar.js
@@ -8,7 +8,7 @@ var ThumbwarSchema   = new Schema({
   body: { type: String, required: true },
   subjectText: { type: String, required: true },
   assertion: { type: String, required: true, index: true },
-  outcome: { type: String, required: true, index: true },
+  outcome: { type: String, index: true },
   subject: {type: Schema.Types.ObjectId, ref: 'User'},
   creator: {type: Schema.Types.ObjectId, ref: 'User'}
 });
