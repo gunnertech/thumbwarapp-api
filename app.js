2b83aafc9873a05d0c3c33943cc2019327e11e8c
Cody Swann <cody@gunnertech.com>
2015-12-16 17:54:35 -0500

    minor

--- app.js
+++ app.js
@@ -70,13 +70,16 @@ app.use(methodOverride(function(req, res){
 
 app.use(function(req, res, next) {
   if(req.query && req.query.authToken) {
+    console.log("LETS FIND BY TOKEN")
     User.findByToken(req.query.authToken,function(err,user){
       
       if(err){ next(err); }
       else {
+        console.log("LETS SET THE USER")
         req.currentUser = user;
         console.log(req.params);
         if(req.params && req.params.userId == "me") {
+          console.log("ALL DONE" + req.currentUser._id)
           req.params.userId = req.currentUser._id;
         }
         
