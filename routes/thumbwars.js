5e20e9dd79f008bcf7397deb23bedbb2213f6d96
Cody Swann <cody@gunnertech.com>
2015-12-19 11:48:28 -0500

    minor

--- routes/thumbwars.js
+++ routes/thumbwars.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
     delete req.query.pagination
     
     req.query.createdAt = {
-      $lt: convertDateToUTC(pagination.olderThan)
+      $lt: convertDateToUTC(Date(pagination.olderThan))
     }
   }
   
