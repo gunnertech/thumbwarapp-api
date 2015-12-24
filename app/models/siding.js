c1cb660bddc7e3a341ac97c950285851650b3191
Cody Swann <cody@gunnertech.com>
2015-12-24 00:51:24 -0500

    minor

--- app/models/siding.js
+++ app/models/siding.js
@@ -12,11 +12,11 @@ var SidingSchema   = new Schema({
 SidingSchema.set('toJSON', { getters: true, virtuals: true });
 
 SidingSchema.virtual('thumbwarId').get(function () {
-  return thumbwar.creator ? thumbwar._id : thumbwar;
+  return this.thumbwar.creator ? thumbwar._id : thumbwar;
 });
 
 SidingSchema.virtual('userId').get(function () {
-  return user.name ? user._id : user;
+  return this.user.name ? user._id : user;
 });
 
 
