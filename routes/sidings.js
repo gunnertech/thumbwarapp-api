1da5bc6facb2c498af582d5079b777174720e683
Cody Swann <cody@gunnertech.com>
2015-12-24 01:28:27 -0500

    minor

--- routes/sidings.js
+++ routes/sidings.js
@@ -13,12 +13,12 @@ router.post('/', function(req, res) {
   
   Thumbwar.findById(req.body.thumbwar).exec()
   .then(function(t){
-    thumbwar =  t;
+    thumbwar = t;
     return Siding.create(req.body)
   })
   .then(function(s){
     siding = s
-    thumbwar.sidings.append(siding);
+    thumbwar.sidings.push(siding);
     return thumbwar.save()
   })
   .then(function(thumbwar){
