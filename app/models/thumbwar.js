60fc183ece4089e2a15be25b05156d63dcb4e56f
Cody Swann <cody@gunnertech.com>
2015-12-31 17:32:15 -0500

    minor

--- app/models/thumbwar.js
+++ app/models/thumbwar.js
@@ -62,7 +62,7 @@ ThumbwarSchema.post('save', function(doc) {
   var Activity = require('./activity');
   var Following = require('./following');
   
-  if(doc.subject && !doc.subject.equals(doc.creator)) {
+  if(doc.subject._id && !doc.subject.equals(doc.creator._id)) {
     Activity.create({
       isAnonymous: doc.isAnonymous,
       body: "challenged you to a Thumbwar!",
